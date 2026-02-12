8a331184a0f0fee4113436a1c1661eb57a31d707
angeloren92 <angeloren92@gmail.com>
2026-02-12 17:08:17 +0100

    get nodes output

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 
 */
 
-//recuperiamo i nodi
+//recuperiamo i nodi del form
 const formEl = document.querySelector('form');
 const fullNameEl = document.getElementById('fullName');
 const distanceEl = document.getElementById('distance');
@@ -19,9 +19,11 @@ const ticket = {
     price: 0.21,
     discount: 1
 }
+
 //evento del form che cattura i value degli input
 formEl.addEventListener('submit', function (e) {
     e.preventDefault();
+    //recuperiamo nell'array i valori degli input 
     ticket.fullNameValue = fullNameEl.value;
     ticket.distanceValue = distanceEl.value;
     ticket.ageValue = ageEl.value;
@@ -31,13 +33,19 @@ formEl.addEventListener('submit', function (e) {
     } else if (ticket.ageValue === 'Over 65') {
         ticket.discount = 0.6;
     }
+    //calcola il prezzo del biglietto
     ticket.price = (ticket.price * ticket.distanceValue * ticket.discount);
 })
-
-
-
-
-
-
 //debug
 console.log(ticket);
+
+//inserimento valori trovati in HTML
+//recuperiamo i nodi dell'output
+const passengerEl = document.querySelector('.passenger');
+const promotionEl = document.querySelector('td:first-child');
+const ticketPriceEl = document.querySelector('td:last-child')
+
+//debug 
+console.log(promotionEl, ticketPriceEl)
+
+
