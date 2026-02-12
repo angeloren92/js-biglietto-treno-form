9818e8922a7c9e7ce0ff91a7c9ba474c794cd8d7
angeloren92 <angeloren92@gmail.com>
2026-02-12 18:39:10 +0100

    defined functions for calculating the process

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ function getInputNodes() {
 function getInputValues(arr) {
     const values = {
     fullNameValue: arr.fullNameEl.value,
-    distanceValue: arr.distanceEl.value,
+    distanceValue: parseInt(arr.distanceEl.value),
     ageValue: arr.ageEl.value
     }
     return values;
@@ -30,35 +30,47 @@ function getInputValues(arr) {
 /**
  * ## Calcolo sconto del biglietto
  * @param {arr} ticket - Inserire L'array conentenenti le informazioni del ticket  
- * @returns restituisce arr con il moltiplicatore del valore al netto dello sconto, se 1 prezzo pieno, 0.8 equivale a 20% e 0.6 il 40% di sconto
+ * @returns restituisce un elemento arr con il moltiplicatore del valore al netto dello sconto, se 1 prezzo pieno, 0.8 equivale a 20% e 0.6 il 40% di sconto
  */
-function calcDiscount(ticket) {
-    ticket.discount = 1;
+function calcDiscount(arr) {
+    discount = 1;
     //calcola lo sconto da applicare
-    if (ticket.ageValue === 'Minorenne') {
-        ticket.discount = 0.8;
-    } else if (ticket.ageValue === 'Over 65') {
-        ticket.discount = 0.6;
+    if (arr.ageValue === 'Minorenne') {
+        discount = 0.8;
+    } else if (arr.ageValue === 'Over 65') {
+        discount = 0.6;
     }
-    console.log(ticket)
-    return ticket;
+    console.log(arr)
+    return discount;
 }
 
-function calcFinalPrice() {
-    ticket.price = (ticket.price * ticket.distanceValue * ticket.discount);
+/**
+ * 
+ * @param {number} val1 - Inserisci il valore della distanza da percorrere
+ * @param {number} val2 - inserisci il valore dello scontro
+ * @returns restituisce un elemento arr con il prezzo finalme calcolato
+ */
+function calcFinalPrice(val1, val2) {
+    const arr = [];
+    arr.finalPrice = (0.21 * val1 * val2);
+    return arr.finalPrice;
 }
 
 
 const formEl = document.querySelector('form');
-//evento del form che cattura i value degli input
+//evento del form
 formEl.addEventListener('submit', function (e) {
+    //blocchiamo il refresh
     e.preventDefault();
+    //catturiamo i nodi di input
     const inputNodes = getInputNodes();
+    //catturiamo i valori dei nodi di input
     const ticket = getInputValues(inputNodes);
-    ticket.price = 0.21;
-    calcDiscount(ticket);
-
-   
+    //calcoliamo lo sconto in base all'età
+    ticket.discount = calcDiscount(ticket.ageValue);
+    //calcoliamo il prezzo finale del biglietto
+    ticket.finalPrice = calcFinalPrice(ticket.discount, ticket.distanceValue)
+    console.log(ticket);
 })
 
 //inserimento valori trovati in HTML
