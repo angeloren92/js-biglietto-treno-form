0b8789795fea4705e2e514a15a62f1a4d5479064
angeloren92 <angeloren92@gmail.com>
2026-02-12 16:24:29 +0100

    get inputs value

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
 /*
 -Recuperiamo in delle variabili i nodi del form e di tutti gli input
 -Evento submit con funzione dove andiamo a bloccare il refresh della pagina
-    -Associamo i value dei vari nodi input dentro alle 
+    -Associamo i value dei vari nodi input dentro alle variabili
 
 */
 
 //recuperiamo i nodi
-const form = document.querySelector('form');
-const fullName = document.getElementById('fullName');
-const distance = document.getElementById('distance');
-const age = document.getElementById('age');
-const btnSubmit = document.querySelector('button[type="submit"]');
-//debug
-console.log(form, fullName, distance, age, btnSubmit)
-//
+const formEl = document.querySelector('form');
+const fullNameEl = document.getElementById('fullName');
+const distanceEl = document.getElementById('distance');
+const ageEl = document.getElementById('age');
+const btnSubmitEl = document.querySelector('button[type="submit"]');
+//array dove salviamo i value del form
+const ticket = {
+    fullNameValue: '',
+    distanceValue: 0,
+    ageValue: ''
+}
+//evento del form che cattura i value degli input
+formEl.addEventListener('submit', function(e){
+    e.preventDefault();
+    ticket.fullNameValue = fullNameEl.value;
+    ticket.distanceValue = distanceEl.value;
+    ticket.ageValue = ageEl.value;
+})
+
 
