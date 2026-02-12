7d45d0826e7880542e1ceefa4369a3cb82833096
angeloren92 <angeloren92@gmail.com>
2026-02-12 15:49:45 +0100

    get nodes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 /*
-Dobbiamo 
+-Recuperiamo in delle variabili i nodi del form e di tutti gli input
+-Evento submit con funzione dove andiamo a bloccare il refresh della pagina
+    -Associamo i value dei vari nodi input dentro alle 
+
 */
+
+//recuperiamo i nodi
+const form = document.querySelector('form');
+const fullName = document.getElementById('fullName');
+const distance = document.getElementById('distance');
+const age = document.getElementById('age');
+const btnSubmit = document.querySelector('button[type="submit"]');
+//debug
+console.log(form, fullName, distance, age, btnSubmit)
+//
+
